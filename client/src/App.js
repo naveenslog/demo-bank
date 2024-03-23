941e3b69af4c95596727fea0de36d38ab3c5d649
Sumit Saurav <impossibleshadowstorm@gmail.com>
2024-03-23 16:53:28 +0530

    feat: added beneficiary page...

--- client/src/App.js
+++ client/src/App.js
@@ -3,38 +3,39 @@ import PartnerSection from "./components/partner";
 import Layout from "./components/shared/layout";
 import Login from "./pages/login";
 import Signup from "./pages/signup";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingSection from "./components/landing";
 import PopularOffers from "./components/popularOffer";
 import Services from "./components/services";
 import PopularProduct from "./components/popularProduct";
 import Dashboard from "./pages/dashboard";
+import "./index.css";
+import Beneficiaries from "./pages/beneficiaries";
 
 const Homepage = () => {
   return (
     <>
       <NavBar />
-      <LandingSection/>
-      <PopularProduct/>
-      <PopularOffers/>
-      <Services/>
+      <LandingSection />
+      <PopularProduct />
+      <PopularOffers />
+      <Services />
       <PartnerSection />
     </>
-  )
-}
+  );
+};
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/dashboard" element={<Layout />}>
-          <Route index element={<Dashboard />} />
-          {/* <Route path="/privacy" element={<Privacy />} />
-          <Route path="/tos" element={<Tos />} /> */}
+        <Route index element={<Homepage />} />
+        <Route path="/" element={<Layout />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Signup />} />
+          <Route path="/beneficiaries" element={<Beneficiaries />} />
         </Route>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Signup />} />
       </Routes>
     </Router>
   );
--- client/src/components/shared/sidebar.jsx
+++ client/src/components/shared/sidebar.jsx
@@ -13,8 +13,7 @@ const Sidebar = () => {
     return (
       <div
         onClick={() => {
-          // setter((oldVal) => !oldVal);
-          navigate(route); // Use history.push for navigation
+          navigate(route);
         }}
         className={`flex gap-1 [&>*]:my-auto text-md pl-6 py-3 border-b-[1px] border-b-white/10 ${colorClass}`}
       >
@@ -29,20 +28,42 @@ const Sidebar = () => {
       <div
         // className={`${className}${appendClass}`}
         style={{
-          width: "30vw",
+          width: "20vw",
           height: "100vh",
           overflowY: "scroll",
-          backgroundColor: "red",
         }}
       >
-        {[
+        {/* {[
           { icon: "home", name: "Home", route: "/" },
           { icon: "dashboard", name: "Dashboard", route: "/dashboard" },
           { icon: "login", name: "Login", route: "/login" },
           { icon: "signup", name: "signup", route: "/register" },
         ].map((item) => (
           <MenuItem icon={item?.icon} name={item?.name} route={item?.route} />
-        ))}
+        ))} */}
+        <nav id="sidebar">
+          <div class="sidebar-header">
+            <h3>Demo Bank</h3>
+          </div>
+
+          <ul class="list-unstyled components">
+            <li class="active">
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/dashboard">Dashboard</a>
+            </li>
+            <li>
+              <a href="/beneficiaries">Beneficiaries</a>
+            </li>
+            <li>
+              <a href="/login">Login</a>
+            </li>
+            <li>
+              <a href="/register">Signup</a>
+            </li>
+          </ul>
+        </nav>
       </div>
     </>
   );
